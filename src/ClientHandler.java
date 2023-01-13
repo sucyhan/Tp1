6338c83ba045d1cfb261af4691ef0eca7559dc2d
sucyhan <sucy.han@polymtl.ca>
2023-01-12 19:24:18 -0500

    Code du prof marche

---
+++ src/ClientHandler.java
@@ -0,0 +1,26 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler extends Thread { // pour traiter la demande de chaque client sur un socket particulier
+	private Socket socket;
+	private int clientNumber;
+
+	public ClientHandler(Socket socket, int clientNumber) {
+		this.socket = socket;
+		this.clientNumber = clientNumber;
+		System.out.println("New connection with client#" + clientNumber + " at" + socket);
+	}
+
+	public void run() { 
+		try {
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // création de canal d’envoi out.writeUTF("Hello from server - you are client#" + clientNumber); // envoi de message} catch (IOException e) {
+System.out.println("Error handling client# " + clientNumber + ": " + e);
+} finally {
+try {
+socket.close();
+} catch (IOException e) {
+System.out.println("Couldn't close a socket, what's going on?");}
+System.out.println("Connection with client# " + clientNumber+ " closed");}
+}
+}
--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,27 @@
-
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 public class Server {
-	public static void main(String[] args)
-	{
-		System.out.println("Hello, World");
+	private static ServerSocket Listener; 
+	public static void main(String[] args) throws Exception {
+	int clientNumber = 0;
+	String serverAddress = "127.0.0.1";
+	int serverPort = 5000;
+	Listener = new ServerSocket();
+	Listener.setReuseAddress(true);
+	InetAddress serverIP = InetAddress.getByName(serverAddress);
+	Listener.bind(new InetSocketAddress(serverIP, serverPort));
+	System.out.format("The server is running on %s:%d%n", serverAddress, serverPort);
+	try {
+	// À chaque fois qu'un nouveau client se, connecte, on exécute la fonstion
+	// run() de l'objet ClientHandler
+	while (true) {
+	// Important : la fonction accept() est bloquante: attend qu'un prochain client se connecte
+	// Une nouvetle connection : on incémente le compteur clientNumber new ClientHandler(Listener.accept(), clientNumber++).start();
 	}
+	} finally {
+	// Fermeture de la connexion
+	Listener.close();
+	} }
 
 }
